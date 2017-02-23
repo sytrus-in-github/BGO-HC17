4280a55bf91ddaee3e00a97556fd9e75582318fb
sytrus-in-github <sytrus-in-github@users.noreply.github.com>
2017-02-23 21:56:57 +0100

    update

--- round1/src/heuristic.py
+++ round1/src/heuristic.py
@@ -2,14 +2,15 @@ import numpy as np
 
 inDir = '../in/'
 fileNames = ['big_trending_today.in','large_kittens.in','medium_videos_worth_spreading.in','small_me_at_the_zoo.in']
+fileName = fileNames[3]
 
-def readInput(filename):
+def readInput():
     # read input file output:
     # list of videos (size)
     # list of enpoints (default time, list((cache, time)))
     # list of caches (list(endpoint))
     # list of requests (vid, ep, number)
-    with open(inDir+filename,'r') as fyle:
+    with open(inDir+fileName,'r') as fyle:
         l = fyle.readline()
         # read first line, initialize lists
         nbVid, nbEp, nbReq, nbCache, cacheSize = [int(i) for i in l.split()]
@@ -37,4 +38,44 @@ def readInput(filename):
 
         return vidSizes, eps, caches, requests, cacheSize
         
+def rankAll(spupMat, nbepMat, nbep, regCoeff):
+    # return ranking and score as matrices of shape (nbcache, nbvid)
+    nbcache = len(nbepMat)
+    # score function !
+    score = lambda spup, ep: spup - regCoeff * ep / nbep
+    # compute and store list of list of scores
+    scores = []
+    for i in range(nbcache):
+        scores.append([score(s, e) for (s, e) in zip(spupMat[i,:], nbepMat[i,:])])
+    # sort list per cache
+    ranks = []
+    skores = [] # permuted scores
+    for l in scores:
+        lstPair = sorted(enumerate(l), key=lambda x: x[0])
+        ranks.append([x[0] for x in lstPair])
+        skores.append([x[1] for x in lstPair])
+    return ranks, skores
+    
+def cacheFillAll(ranks, scores, vidSizes, cacheMem):
+    # naive fill-up of caches
+    nbcache = len(ranks)
+    fills = [[]] * nbcache
+    for i in range(nbcache):
+        availableMem = cacheMem
+        for r in ranks[i]:
+            if vidSizes[r] <= availableMem:
+                fills[i].append(r)
+                availableMem -= vidSizes[r]
+    return fills
+
+outDir = '../out/'
+    
+def outputFile(fills):
+    usefulfills = [l for l in fills in l]
+    nbuseful = len(usefulfills)
+    with open(outDir+fileName,'w') as out:
+        out.write(str(nbuseful)+'\n')
+        for l in usefulfills:
+            s = ''
+    
 print(readInput(fileNames[3]))
